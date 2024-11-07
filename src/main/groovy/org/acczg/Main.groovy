e28381ebcf8d5e7fa3b67b801ff01bfa979f1792
vhprestes <vhprestes@gmail.com>
2024-11-07 12:06:18 -0300

    feat: calling method on main

--- src/main/groovy/org/acczg/Main.groovy
+++ src/main/groovy/org/acczg/Main.groovy
@@ -1,5 +1,21 @@
 package org.acczg
 
+import org.acczg.crawler.Crawler
+import org.acczg.utils.LyricsAnalyzer
+
 static void main(String[] args) {
     println "Hello world!"
+    def crawler = new Crawler()
+    def lyrics = crawler.getLyrics()
+
+
+
+    LyricsAnalyzer lyricsAnalyzer = new LyricsAnalyzer()
+    println "Ditongos: " + lyricsAnalyzer.findWordsWithDitongos(lyrics)
+    println "Tritongos: " + lyricsAnalyzer.findWordsWithTritongos(lyrics)
+    println "Hiatos: " + lyricsAnalyzer.findWordsWithHiatos(lyrics)
+    println "Proparoxytonas: " + lyricsAnalyzer.findProparoxytonas(lyrics)
+    println "Sentences with four words: " + lyricsAnalyzer.findFourWordSentences(lyrics)
+
+    println lyrics
 }
