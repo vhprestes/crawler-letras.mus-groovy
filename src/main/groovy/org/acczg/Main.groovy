b91fe47c401cfa69dd5ae41d4888720f531f86cd
vhprestes <vhprestes@gmail.com>
2024-11-07 19:42:32 -0300

    refactor: update to factory method

--- src/main/groovy/org/acczg/Main.groovy
+++ src/main/groovy/org/acczg/Main.groovy
@@ -2,18 +2,17 @@ package org.acczg
 
 import org.acczg.crawler.Crawler
 import org.acczg.utils.LyricsAnalyzer
+import org.acczg.view.FilterMenu
 
 static void main(String[] args) {
     println "Hello world!"
     def crawler = new Crawler()
     def lyrics = crawler.getLyrics()
 
-    LyricsAnalyzer lyricsAnalyzer = new LyricsAnalyzer()
-    println "Ditongos: " + lyricsAnalyzer.findWordsWithDitongos(lyrics)
-    println "Tritongos: " + lyricsAnalyzer.findWordsWithTritongos(lyrics)
-    println "Hiatos: " + lyricsAnalyzer.findWordsWithHiatos(lyrics)
-    println "Proparoxytonas: " + lyricsAnalyzer.findProparoxitonas(lyrics)
-    println "Sentences with four words: " + lyricsAnalyzer.findFourWordSentences(lyrics)
+    FilterMenu filterMenu = new FilterMenu()
+    filterMenu.showMenu(lyrics)
 
-    println lyrics
-}
+
+
+    }
+
