d9954c2cb9e4176dbb2ab1be84a2f6f04a63488b
vhprestes <vhprestes@gmail.com>
2024-11-07 12:05:16 -0300

    feat: temporary crawler with hard coded url

--- src/main/groovy/org/acczg/crawler/Crawler.groovy
+++ src/main/groovy/org/acczg/crawler/Crawler.groovy
@@ -1,7 +1,17 @@
 package org.acczg.crawler
 
+import org.jsoup.nodes.Document
+import org.jsoup.Jsoup
+
 class Crawler {
 
 
     String url = "https://www.letras.mus.br/chico-buarque/45124/"
+
+
+    def getLyrics() {
+        Document doc = Jsoup.connect(url).get()
+        def lyrics = doc.select(".lyric-original").text()
+        return lyrics
+    }
 }
